5bfad60027d1d5395b61eb5a1a5cc52e241157c0
Waimoe21 <117363601+Waimoe21@users.noreply.github.com>
2026-05-17 22:17:09 +0630

    Update 1_Overview.py

--- pages/1_Overview.py
+++ pages/1_Overview.py
@@ -56,9 +56,10 @@ st.title("Overview")
 col1, col2, col3 = st.columns([0.5, 3, 0.5])
 
 with col2:
-    banner_path = os.path.join("pages", "banner.png")
+    banner_path = os.path.join(BASE_DIR, "banner.png")
+    banner_path = os.path.normpath(banner_path)
 
-    if os.path.exists(banner_path):
+    if os.path.isfile(banner_path):
         st.image(banner_path, use_container_width=True)
     else:
         st.error(f"Missing file: {banner_path}")
